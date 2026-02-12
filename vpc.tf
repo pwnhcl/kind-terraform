07a10624f9bea10bf98ac5bb5f8ae7f9ae49274b
Pawan Kumar <pwnhcl@gmail.com>
2026-02-13 00:42:24 +0530

    added port 8080

--- vpc.tf
+++ vpc.tf
@@ -55,6 +55,14 @@ resource "aws_security_group" "allow_ssh_http" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+    ingress {
+      description = "HTTPS"
+      from_port = 8080
+      to_port = 8080
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+
+  }
 
 
   egress {
